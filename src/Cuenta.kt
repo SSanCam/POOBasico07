7c1c2d1b2414c7179837bc7f5263db51a041b6f9
Sara S Camilleri <ssancam391@g.educaand.es>
2024-02-27 11:52:55 +0100

    doc_comentarios

--- src/Cuenta.kt
+++ src/Cuenta.kt
@@ -1,3 +1,8 @@
+/**
+ * Clase que representa una cuenta bancaria.
+ * @property numeroCuenta El número de la cuenta bancaria, debe constar de 9 dígitos.
+ * @property saldoDisponible El saldo disponible en la cuenta.
+ */
 class Cuenta(numeroCuenta: Int, var saldoDisponible: Double) {
 
     var numeroCuenta: Int = numeroCuenta
@@ -6,14 +11,29 @@ class Cuenta(numeroCuenta: Int, var saldoDisponible: Double) {
             field = value
         }
 
+    /**
+     * Consulta el saldo disponible en la cuenta.
+     * @return Un string representando el saldo disponible.
+     */
     fun consultarSaldo(): String {
         return "Saldo disponible: $saldoDisponible"
     }
 
+    /**
+     * Recibe un abono en la cuenta.
+     * @param cantidad El monto del abono.
+     * @return El nuevo saldo disponible después del abono.
+     */
     fun recibirAbono(cantidad: Double): Double {
+
         return saldoDisponible + cantidad
     }
 
+    /**
+     * Realiza un pago desde la cuenta.
+     * @param pago El monto a pagar.
+     * @return El nuevo saldo disponible después del pago.
+     */
     fun realizarPago(pago: Double): Double {
         return if (saldoDisponible - pago <= 0.0) {
             0.0
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,31 @@
 fun main() {
 
+    try{
+        /*instanciar un objeto Persona con un DNI cualquiera, así como dos objetos cuenta,
+ una sin saldo inicial y otra con 700 euros.*/
+        val cuenta1P1 = Cuenta(333444222, 0.0)
+        val cuenta2P1 = Cuenta(222111333, 700.00)
+        val persona1 = Persona("123123123", arrayOf(cuenta1P1, cuenta2P1))
+
+        /*La persona recibe la nómina mensual, por lo que ingresa 1100 euros en la primera cuenta,
+        pero tiene que pagar el alquiler de 750 euros con la segunda. Imprimir por pantalla si la persona es morosa.
+        */
+        var saldoCuenta1 = persona1.cuentas[0]
+        var saldoCuenta2 = persona1.cuentas[1]
+        println("El saldo en la primera cuenta es de: $saldoCuenta1€")
+        println("El saldo en la segunda cuenta es de: $saldoCuenta2€")
+
+        val ingresoNomina = persona1.cuentas[0].recibirAbono(1100.0)
+        println("Has recibido tu nómina en tu cuenta principal. Tu saldo actual es de: ")
+        println(saldoCuenta1)
+
+        val pagarAlquiler = persona1.cuentas[1].realizarPago(750.0)
+        println(pagarAlquiler)
+        //val transferencia = persona1.transferencia(persona1,cuenta1P1,persona1,cuenta2P1, 50.0)
+
+    }catch (e: IllegalArgumentException){
+        println(e.message)
+    }
+
+
 }
